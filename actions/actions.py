34bf017bf3e8e3b00aa2e8deb4551df83897e720
Hitesh Tare <hiteshtare@gmail.com>
2021-06-17 16:52:08 +0530

    Slot from Custom action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,38 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("name")
+        country = tracker.get_slot("country")
+
+        leader_name = ''
+        if country.lower() == 'america':
+            leader_name = 'Joe Biden'
+        elif country.lower() == 'india':
+            leader_name = 'Mr. Modi'
+        else:
+            leader_name = "Leader name not found in db."
+
+        message = name + ' belongs to ' + country + ' and leader name is ' + leader_name
+        print(message)
+
+        dispatcher.utter_message(text=message)
+
+        return [SlotSet("leader", leader_name)]
